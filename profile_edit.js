bf983f8911f97803bcaceda417326a63cec07c6a
AMinSC <cdtjdcjf@gmail.com>
2023-08-31 14:49:12 +0900

    회원정보 수정 비밀번호 체크 검사 중

--- profile_edit.js
+++ profile_edit.js
@@ -3,6 +3,18 @@ const activityPointInput = document.getElementById('activity_point');
 const profileImgInput = document.getElementById('profile_img');
 // ... Other fields ...
 
+// async function handleResponseError(error) {
+//     if (error.response.status === 401) {
+//         alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+//         // 필요하다면 로그인 페이지로 리디렉션
+//         localStorage.removeItem('accessToken');
+//         window.location.href = 'login.html';
+//     } else {
+//         console.log(error);
+//         alert('오류가 발생했습니다.');
+//     }
+// }
+
 async function fetchProfile() {
     try {
         const response = await axios.get('http://localhost/player/update/', {
@@ -24,11 +36,26 @@ async function fetchProfile() {
 async function submitProfile() {
     try {
         const formData = new FormData();
-        formData.append('profile_img', profileImgInput.files[0]);
         formData.append('age', document.getElementById('age').value);
-        // ... Append other fields ...
+        formData.append('gender', document.getElementById('gender').value);
+        formData.append('category', document.getElementById('category').value);
+        formData.append('position', document.getElementById('position').value);
 
-        const response = await axios.put('http://localhost/player/update/', formData);
+        const password = document.getElementById('currentPassword').value;
+        const profileImg = profileImgInput.files[0];
+
+        if (password) {
+            formData.append('password', password);
+        }
+        if (profileImg) {
+            formData.append('profile_img', profileImg);
+        }
+
+        const response = await axios.put('http://localhost/player/update/', formData, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        });
 
         if (response.status === 200) {
             alert('회원정보가 성공적으로 수정되었습니다.');
@@ -36,13 +63,22 @@ async function submitProfile() {
             alert('회원정보 수정에 실패하였습니다.');
         }
     } catch (error) {
-        console.error('Failed to update profile:', error);
+        if (error.response && error.response.data) {
+            const errorMessage = error.response.data;
+            if (errorMessage.password) {
+                alert(errorMessage.password[0]);
+            } else {
+                console.error('Failed to update profile:', error);
+            }
+        } else {
+            console.error('Failed to update profile:', error);
+        }
     }
 }
 
 function goToPasswordChange() {
     // Redirect to password change page
-    window.location.href = '/path/to/password/change';
+    window.location.href = 'update-ps.html';
 }
 
 // Fetch profile on page load
---
+++ update-ps.js
@@ -0,0 +1,25 @@
+document.getElementById('passwordChangeForm').addEventListener('submit', async function(e) {
+    e.preventDefault();
+    try {
+        const currentPassword = document.getElementById('currentPassword').value;
+        const newPassword = document.getElementById('newPassword').value;
+
+        const response = await axios.put('http://localhost/player/update/ps/', {
+            current_password: currentPassword,
+            new_password: newPassword
+        }, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        });
+
+        if (response.status === 200) {
+            alert('비밀번호가 성공적으로 변경되었습니다.');
+            document.location.href = "profile_edit.html"
+        } else {
+            alert('비밀번호 변경에 실패하였습니다.');
+        }
+    } catch (error) {
+        console.error('Failed to change password:', error);
+    }
+});
